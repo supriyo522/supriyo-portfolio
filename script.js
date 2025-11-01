07b1ae775bd7702fe910a712e14894ef09f8e2c4
Supriyo <supriyo@MacBookAir.lan>
2025-11-02 01:37:47 +0530

    first commits

--- script.js
+++ script.js
@@ -14,4 +14,9 @@ hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
   hamburger.classList.toggle("active");
 });
-
+document.addEventListener('contextmenu', event => event.preventDefault());
+  document.onkeydown = function(e) {
+    if (e.keyCode == 123) return false; // F12
+    if (e.ctrlKey && e.shiftKey && (e.keyCode == 73 || e.keyCode == 74)) return false; // Ctrl+Shift+I or J
+    if (e.ctrlKey && e.keyCode == 85) return false; // Ctrl+U
+  };
