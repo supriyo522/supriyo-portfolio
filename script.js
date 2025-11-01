ee1ff33514a6708bded9488dc1784ef93e5bee5e
Supriyo <supriyo@MacBookAir.lan>
2025-11-02 01:30:39 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// Smooth scrolling for navigation
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+  hamburger.classList.toggle("active");
+});
+
